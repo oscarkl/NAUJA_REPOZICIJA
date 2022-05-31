52348e3944117f0643a3e9f880f158cac2989785
oscarkl <42514691+oscarkl@users.noreply.github.com>
2022-06-01 02:12:22 +0300

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -8,25 +8,25 @@ bool isNumber(const std::string& str)
     return true;
 }
 
-double vidurkis(const Vektorius<double>&h) {
+double vidurkis(vector<double>& h) {
     double sum = 0;
     for (auto pazymys : h)
     {
         sum += pazymys;
     }
-    return sum / h.Size();
+    return sum / h.size();
 }
 
-double egz(double vid, const Vektorius<double>&h) {
+double egz(double vid, vector<double>& h) {
     double egzas = 0.6 * h.back() + 0.4 * vidurkis(h);
     return egzas;
 }
 
-studentas skaitymas(std::ifstream& fd)
+void skaitymas(std::ifstream& fd, studentas& temp)
 {
     std::string data, vard, pav;
     int sk;
-    Vektorius<double> h;
+    vector<double> h;
     double gal;
     fd >> vard;
     fd >> pav;
@@ -39,29 +39,31 @@ studentas skaitymas(std::ifstream& fd)
         
     }
     gal = egz(vidurkis(h), h);
-    return studentas(vard, pav, gal, h);
+    studentas a(vard, pav, gal, h);
+    temp = a;
+    //std::cout << temp.vardai();
+    //return (studentas(vard, pav, gal, h));
 }
 
-bool rusiavimas(const studentas&lhs, const studentas&rhs) {
+bool rusiavimas(const studentas& lhs, const studentas& rhs) {
     if (lhs.vardai() != rhs.vardai()) return lhs.vardai() < rhs.vardai();
     else return lhs.vardai() < rhs.vardai();
 }
 
 bool compare_5(const studentas& v) { return v.egzas() < 5; }
-/*
-void RemoveRezult(Vektorius<studentas>& ab) {
+void RemoveRezult(vector<studentas>& ab) {
     ab.erase(
         std::remove_if(ab.begin(), ab.end(), [&](studentas const& ab) {
             return compare_5(ab);
             }),
         ab.end());
-}*/
+}
 //copy constructor
 
 studentas::studentas(const studentas& source) {
     vardas = source.vardas;
-    pavarde = source.pavarde;   
-    gal = source.gal;    
+    pavarde = source.pavarde;
+    gal = source.gal;
     n = source.n;
 }
 //overload assignment operator 
@@ -78,13 +80,4 @@ studentas& studentas::operator=(const studentas& source) {
 }
 studentas::~studentas() {
     n.clear();
-}
-std::ostream& operator<<(std::ostream& fr, const studentas& source)
-{
-    fr << std::left << std::setw(16) << source.vardas << std::left << std::setw(16) << source.pavarde;
-    for (int i = 0; i < source.n.Size(); i++) {
-        fr << std::left << std::setw(8) << source.n[i] << " ";
-    }
-    fr << source.gal << std::endl;
-    return fr;
 }
