d9a9d00cfd78713bbbfbd536d2415ca1ccb97862
oscarkl <42514691+oscarkl@users.noreply.github.com>
2022-06-01 02:12:56 +0300

    Update struktura.h

--- struktura.h
+++ struktura.h
@@ -5,12 +5,14 @@
 #include <stdio.h> 
 #include <time.h> 
 #include <string>
-#include <vector>  
 #include <fstream>
 #include <sstream>
 #include <algorithm>
 #include <chrono>
+
 #include "vektorius.h"
+//#include <vector>  
+//using std::vector;
 
 class zmogus {
 public:
@@ -23,24 +25,23 @@ private:
     std::string vardas;
     std::string pavarde;
     double gal;
-    Vektorius<double> n;
-    
+    vector<double> n;
+
 public:
     studentas() : vardas(), pavarde(), gal(), n() { }
-    studentas(const std::string& vardas, const std::string& pavarde, double gal, const Vektorius<double>& n) : vardas(vardas), pavarde(pavarde), gal(gal), n(n) {}
+    studentas(const std::string& vardas, const std::string& pavarde, double gal, const vector<double>& n) : vardas(vardas), pavarde(pavarde), gal(gal), n(n) {}
     const std::string& vardai() const { return vardas; }
     const std::string& pavardes() const { return pavarde; }
-    const Vektorius<double>& pazym() const { return n; }
+    const vector<double>& pazym() const { return n; }
     double egzas() const { return gal; }
     studentas(const studentas& source);
-    studentas& operator=(const studentas& source);    
+    studentas& operator=(const studentas& source);
     ~studentas();
-    friend std::ostream& operator<<(std::ostream& os, const studentas& source);
 };
 
 
-studentas skaitymas(std::ifstream& fd);
+void skaitymas(std::ifstream& fd, studentas& temp);
 bool rusiavimas(const studentas& lhs, const studentas& rhs);
 bool compare_5(const studentas& v);
-void RemoveRezult(Vektorius<studentas>& ab);
+void RemoveRezult(vector<studentas>& ab);
 bool isNumber(const std::string& str);
